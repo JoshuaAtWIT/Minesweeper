b2d18c0e58c07be24225e8aaba5050516f80096c
jjsol <jjsol@10.220.82.90>
2024-04-04 15:57:03 -0400

    Base

---
+++ src/Minesweeper/Minesweeper.java
@@ -0,0 +1,17 @@
+package Minesweeper;
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Minesweeper extends Application {
+	
+	@Override
+	public void start(Stage st) {
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
